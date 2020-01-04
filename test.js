216bee41bef851cf8c38ce6b3ce214e9b9559d43
Denis Efimenko <efimenko.danis@gmail.com>
2020-01-04 23:58:18 +0200

    initial commit

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 'use strict'
+
 function getMaxSubSum(arr) {
-    
+    let value = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0){
+            value += arr[i];
+        }
+    };
+    return value;
 };
-
-
-getMaxSubSum([-1, 2, 3, -9]) //= 5 (сумма выделенных)
-getMaxSubSum([2, -1, 2, 3, -9]) //= 6
-getMaxSubSum([-1, 2, 3, -9, 11])// = 11
-getMaxSubSum([-2, -1, 1, 2])// = 3
-getMaxSubSum([100, -9, 2, -3, 5])// = 100
-getMaxSubSum([1, 2, 3]) //= 6 (берём все)
-getMaxSubSum([1, -2, 3, 4, -9, 6])
+console.log(getMaxSubSum([-1, 2, 3, -9])) //= 5 (сумма выделенных)
+console.log(getMaxSubSum([2, -1, 2, 3, -9])) //= 6
+console.log(getMaxSubSum([-1, 2, 3, -9, 11]))// = 11
+console.log(getMaxSubSum([-2, -1, 1, 2]))// = 3
+console.log(getMaxSubSum([100, -9, 2, -3, 5]))// = 100
+console.log(getMaxSubSum([1, 2, 3])) //= 6 (берём все)
+console.log(getMaxSubSum([1, -2, 3, 4, -9, 6]))//=7
