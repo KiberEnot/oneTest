408c6ef44a8f0261be2a89a2af7f2bba22a124d7
Denis Efimenko <efimenko.danis@gmail.com>
2020-01-09 23:13:19 +0200

    commit

--- test.js
+++ test.js
@@ -1,22 +1,20 @@
 'use strict'
 
-function getMaxSubSum(arr) {
-    let maxSum = 0;
-    let sum = 0;
-    for (let i of arr) {
-        sum += i;
-        if (sum < 0) {
-            sum = 0;
-        } else if (sum > maxSum){
-            maxSum = sum;
-        };
+function Calculator(){
+    this['+'] = (a, b) => a + b;
+    this['-'] = (a, b) => a - b; 
+    this.calculate = (str) => {
+        str.split(' ')[1] == 
     };
-    return maxSum;
 };
-console.log(getMaxSubSum([-1, 2, 3, -9])) //= 5 (сумма выделенных)
-console.log(getMaxSubSum([2, -1, 2, 3, -9])) //= 6
-console.log(getMaxSubSum([-1, 2, 3, -9, 11]))// = 11
-console.log(getMaxSubSum([-2, -1, 1, 2]))// = 3
-console.log(getMaxSubSum([100, -9, 2, -3, 5]))// = 100
-console.log(getMaxSubSum([1, 2, 3])) //= 6 (берём все)
-console.log(getMaxSubSum([1, -2, 3, 4, -9, 6]))//=7
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+//let powerCalc = new Calculator;
+//powerCalc.addMethod("*", (a, b) => a * b);
+//powerCalc.addMethod("/", (a, b) => a / b);
+//powerCalc.addMethod("**", (a, b) => a ** b);
+
+//let result = powerCalc.calculate("2 ** 3");
+//alert( result ); // 8
